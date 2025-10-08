aefe81221c1c8ef1beca086ce32a87a66a3f5b5f
rohithreddy878 <rohithreddy878@gmail.com>
2025-10-08 13:05:16 -0500

    css.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="min-h-screen flex flex-col">
         <Navbar />
-        <main className="flex-1 container mx-auto px-4 py-6">{children}</main>
+        <main className="flex-1 container  max-w-full  px-4 py-6">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
