dc00e8455e1b26d2c84ef6dd28c5fd230469821b
rohithreddy878 <rohithreddy878@gmail.com>
2025-10-15 23:45:31 -0500

    bugg

--- components/AutoResizeQuill.tsx
+++ components/AutoResizeQuill.tsx
@@ -5,7 +5,9 @@ import dynamic from "next/dynamic";
 import "react-quill-new/dist/quill.snow.css";
 
 // dynamically import ReactQuill to avoid SSR issues
-const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
+const ReactQuill: any = dynamic(() => import("react-quill-new"), {
+  ssr: false,
+});
 
 export default function AutoResizeQuill({
   value,
